68b8b32f478b72b35571e39e7546b4563b3ac0f9
Oğuz Kumcular <oguzkumcular@gmail.com>
2018-03-19 23:51:38 +0300

    MoveBall, MoveRacket component adding
    MoveBall, MoveRacket component adding. Score will come... coming soon.

---
+++ Assets/MoveBall.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+    public float speed = 5.0f;
+
+    MoveRacket LRacket;
+    MoveRacket RRacket;
+
+    public GameObject LR;
+    public GameObject RR;
+
+	// Use this for initialization
+	void Start () {
+        
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, 0) * speed;
+        LRacket = LR.GetComponent<MoveRacket>();
+        RRacket = RR.GetComponent<MoveRacket>();
+	}
+
+    private void FixedUpdate()
+    {
+
+         
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) //cok onemli.
+    {
+        if (other.gameObject.name == "RightWall")
+        {
+            LRacket.Score++;
+        }
+        if (other.gameObject.name == "LeftWall")
+        {
+            RRacket.Score++;
+           
+        }
+        if(other.gameObject.name == "RightRacket")
+        {
+            float ySub = transform.position.y - other.gameObject.transform.position.y;
+            float RacketHeight = other.collider.bounds.size.y;
+            float y = ySub / RacketHeight;
+            Vector2 direction = new Vector2(-1, y);
+
+            GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+        }
+        if (other.gameObject.name == "LeftRacket")
+        {
+           float ySub = transform.position.y - other.gameObject.transform.position.y;
+           float RacketHeight = other.collider.bounds.size.y;
+           float y = ySub / RacketHeight;
+           Vector2 direction = new Vector2(1, y);
+
+           GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+
+        }
+    }
+
+}
---
+++ Assets/MoveRacket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRacket : MonoBehaviour {
+    public float speed;
+    public float Score;
+    public string VerticalAxis;
+    
+	// Use this for initialization
+	void Start () {
+        speed = 5.0f;
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float y = Input.GetAxisRaw(VerticalAxis); // - + deger verme vertical yataylik. alfa beta cok onemli degil.
+
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, y) * speed;
+    }
+}
